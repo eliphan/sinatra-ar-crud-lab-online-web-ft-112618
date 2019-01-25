089853045d345af406893f299f756c993ed87fb1
Elise Phan <aliceconnic@gmail.com>
2019-01-25 02:38:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   get '/articles/:id/edit' do
     @article = Article.find(params[:id])
     @article.update(params[:article])
-    erb :edit
+    redirect to "/articles/#{ @article.id }"
   end
   
   patch '/articles/:id' do
