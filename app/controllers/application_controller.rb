4c82bf26d9d93e917b039edc37c5440e0eab8479
Elise Phan <aliceconnic@gmail.com>
2019-01-25 01:34:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,13 +31,11 @@ class ApplicationController < Sinatra::Base
   end  
   
 #Active Record will find the article with id number set by the params, grab that article, and show it to the user
-   get 'articles/:id' do
+   get "/articles/:id" do
     @article = Article.find(params[:id])
     erb :show
   end 
-  get "/articles/:id" do
-    @article = Article.find(params[:id])
-    erb :show
+
   end
  get '/' do
     redirect to "/articles"
