da799cc70f1dc3644d0aa998d5826ce76fc8fd55
Elise Phan <aliceconnic@gmail.com>
2019-01-25 00:22:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do
     Article.create(title: params[:title])
-
+    Article.create(content: params[:content])
     erb :index
   end
   
